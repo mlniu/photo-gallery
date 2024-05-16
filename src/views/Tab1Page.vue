eb0b711aaa3ce25dfe9978afac93fb8bea2f8021
MaoLinNiu <x264748749>
2024-05-16 16:30:59 +0800

    test2

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Tab 1 {{ path }} TEST</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
@@ -20,4 +20,8 @@
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+
+const path = Directory.Data;
+console.log('path',path)
 </script>
